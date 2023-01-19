29534f7cd3645b593966ab741fea6f97b5c25d61
Hind El gharbi <75484460+hind14@users.noreply.github.com>
2023-01-19 21:38:28 +0100

    Delete cours-spring-security-db directory

--- cours-spring-security-db/src/main/java/com/example/demo/dao/AdresseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.models.Adresse;
-
-public interface AdresseRepository extends JpaRepository<Adresse, Integer> {
-
-}
--- cours-spring-security-db/src/main/java/com/example/demo/dao/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.demo.models.User;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-	public User findByUsername(String nom);
-}
